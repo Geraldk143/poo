6fab2fc32ae71e5d06e05c540a0e481ba6489194
Geraldk143 <geraldinekhoo143@gmail.com>
2024-05-28 22:36:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 
-@app.route("/bank_website", methods=["GET", "POST"])
-def main():
+@app.route("/bank_website",methods=["GET","POST"])
+def bank_website():
     r = request.form.get("q")
-    return render_template("bank_website.html", r=r)
+    return(render_template("bank_website.html",r=r))
 
-if __name__ == "__main__":
-    app.run
+if __name__ == "__bank_website__":
+    app.run()
