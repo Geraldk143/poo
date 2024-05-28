bd0cfdd5c92c3d84d1cd7616daa9200bde62c3dd
Geraldk143 <geraldinekhoo143@gmail.com>
2024-05-28 22:33:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    return render_template("index.html")
-
 @app.route("/bank_website", methods=["GET", "POST"])
 def main():
     r = request.form.get("q")
